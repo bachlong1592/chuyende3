810abc2bb654bd7e2908bfa3bed068b40256b15c
Trương Văn Trung <113073858+bachlong1592@users.noreply.github.com>
2023-09-10 21:50:15 +0700

    2

--- TrungProject/Trung1/Trung1/settings.py
+++ TrungProject/Trung1/Trung1/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'Trung1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'your_database_name',
+        'USER': 'your_mysql_username',
+        'PASSWORD': 'your_mysql_password',
+        'HOST': 'localhost',  # hoặc địa chỉ IP của MySQL server
+        'PORT': '3306',       # cổng mặc định của MySQL
     }
 }
+
 
 
 # Password validation
